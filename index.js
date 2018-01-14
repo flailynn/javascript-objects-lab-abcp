debc985b92b16659e506c4f468690d61d6612910
Anita Beauchamp <amadamspr@gmail.com>
2018-01-14 01:55:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var recipes = {};
 
-function updateObjectWithKeyAndValue(object,key,value) {
+function destructivelyUpdateObjectWithKeyAndValue(object,key,value) {
   object[key] = value;
   return object;
+}
+
+function updateObjectWithKeyAndValue(object,key,value) {
+  
 }
