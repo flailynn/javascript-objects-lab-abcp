f637ee86244b25af352312aae485e2bdf70f6cf8
Anita Beauchamp <amadamspr@gmail.com>
2018-01-14 01:56:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ function destructivelyUpdateObjectWithKeyAndValue(object,key,value) {
 }
 
 function updateObjectWithKeyAndValue(object,key,value) {
-  return Object.assign({},obj,{key: value});
+  return Object.assign({},object,{key: value});
 }
